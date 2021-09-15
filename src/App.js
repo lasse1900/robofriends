0b96bdef2ced3d337df29402eb14e935add707fa
Lauri Kyttälä <lauri.kyttala@gmail.com>
2021-09-15 11:28:30 +0300

    Robo app build in stage 4a using jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
-import { robots } from './robots';
+// import { robots } from './robots';
 import SearchBox from './SearchBox';
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
+    console.log('cunstructor')
   }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        return response.json();
+      })
+      .then(users => {
+        this.setState({ robots: users });
+      })
+    // this.setState({robots: robots});
+    console.log('componentDidMount')
+  }
+
   // onSearchChange(event) { OBS !!!
   onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value })
@@ -20,19 +34,26 @@ class App extends Component {
     // console.log(filteredRobots);
   }
 
+
   render() {
     const filteredRobots = this.state.robots.filter(robot => {
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
     })
-    return (
-      <div className='tc'>
-        <h1>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-        {/* <CardList robots={this.state.robots} />  */}
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h2>Loading ...</h2>
+    } else {
+      console.log('render')
+      return (
+        <div className='tc'>
+          <h1 className='f2'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+          {/* <CardList robots={this.state.robots} />  */}
+        </div>
+      );
+    }
   }
 }
+
 
 export default App;
