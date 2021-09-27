4e9680c123e5103aedab62d60dd7102fcbadfceb
Lauri Kyttälä <lauri.kyttala@gmail.com>
2021-09-27 17:49:36 +0300

    many changes to get reducer/actions working - now ok

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,48 +5,36 @@ import SearchBox from '../compponents/SearchBox';
 import Scroll from '../compponents/Scroll';
 import ErrorBoundary from '../compponents/ErrorBoundary';
 
-import { setSearchField } from './actions';
+import { setSearchField, requestRobots } from './actions';
 
 const mapStateToProps = state => {
   return {
-    searchField: state.searchField
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error,
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
   }
 }
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      robots: []
-      // searchfield: ''
-    }
-  }
 
   componentDidMount() {
-    // console.log(this.props.store.getState())
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ robots: users }))
+    this.props.onRequestRobots();
   }
 
-  // // onSearchChange(event) { OBS !!!
-  // onSearchChange = (event) => {
-  //   this.setState({ searchfield: event.target.value })
-  // }
-
   render() {
-    const { robots } = this.state;
-    const { searchField, onSearchChange } = this.props;
+    const { searchField, onSearchChange, robots, isPending } = this.props;
     const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchField.toLowerCase());
     })
-    return !robots.length ?
+    return isPending ?
       <h2>Loading ...</h2> :
       (
         <div className='tc'>
